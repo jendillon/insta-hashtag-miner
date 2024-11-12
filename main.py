d1eccee7a2bbafbf059f5667a6be6b1610cb5d2d
Jennifer Dillon <jen.j.dillon@gmail.com>
2024-11-11 22:29:50 -0800

    Added credential management

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from instaloader import instaloader
 from numpy import save
 import csv
+import os
 
 if __name__ == '__main__':
     L = instaloader.Instaloader()
-    # Login information goes here.  
+
+    print(os.environ.keys())
+
+    # Login information goes here.
+    username = os.environ.get('INSTA_LOGIN')
+    password = os.environ.get('INSTA_PASSWORD')
+    L.login(username, password)
+
     profile_name = 'seekingsomaart'
     profile = instaloader.Profile.from_username(L.context, profile_name)
     post_iterator = profile.get_posts()
@@ -12,6 +20,7 @@ if __name__ == '__main__':
     try:
         for post in post_iterator:
             hashtags = post.caption_hashtags
+            print(hashtags)
             for tag in hashtags:
                 if tag not in hashtag_dict:
                     hashtag_dict[tag] = 1
@@ -20,6 +29,8 @@ if __name__ == '__main__':
                     hashtag_dict[tag] = tag_count + 1
     except KeyboardInterrupt:
         save("dict.json", post_iterator.freeze())
+
+    hashtag_dict.keys.sort()
 
     with open('dict.csv', 'w') as csv_file:
         writer = csv.writer(csv_file)
