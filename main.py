1b9bcff046de0b5edcde239d3fbaa395618b0842
Jennifer Dillon <jen.j.dillon@gmail.com>
2024-11-18 20:56:56 -0800

    Small refactor and fixing more linting errors

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import csv
 import os
 from instaloader import instaloader
+PROFILE_NAME = 'seekingsomaart'
 
-if __name__ == '__main__':
-    L = instaloader.Instaloader()
+def save_sorted_csv( unsorted_dict ):
+    """
+    Takes a dictionary, sorts it, and writes it to a .csv 
+    """""
+    sorted_hashtag_dict = dict(sorted(unsorted_dict.items()))
+    with open('dict.csv', 'w', encoding="utf-8") as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in sorted_hashtag_dict.items():
+            writer.writerow([key, value])
 
-    # Login information goes here.
+def login():
+    """
+    Logs into instagram using locally stored credentials
+    """
+    loader = instaloader.Instaloader()
     username = os.environ.get('INSTA_LOGIN')
     password = os.environ.get('INSTA_PASSWORD')
     L.login(username, password)
+    return loader
 
-    PROFILE_NAME = 'seekingsomaart'
+if __name__ == '__main__':
+    """Using locally stored instagram credentials, counts instances of 
+    hashtags on a given instagram account, and outputs them to a .csv"""
+
+    L = login()
     profile = instaloader.Profile.from_username(L.context, PROFILE_NAME)
     post_iterator = profile.get_posts()
+
     hashtag_dict = {}
     try:
         for post in post_iterator:
@@ -25,11 +43,8 @@ if __name__ == '__main__':
                     tag_count = hashtag_dict[tag]
                     hashtag_dict[tag] = tag_count + 1
     except KeyboardInterrupt:
-        save("dict.json", post_iterator.freeze())
+        save_sorted_csv(hashtag_dict)
+    finally:
+        save_sorted_csv(hashtag_dict)
 
-    sorted_hashtag_dict = dict(sorted(hashtag_dict.items()))
 
-    with open('dict.csv', 'w', encoding="utf-8") as csv_file:
-        writer = csv.writer(csv_file)
-        for key, value in sorted_hashtag_dict.items():
-            writer.writerow([key, value])
